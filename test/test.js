2ad04ff72613ad999b6fffb81c7c0af6cefdf992
Abner丶Lee <408666378@qq.com>
2017-04-13 15:24:24 +0800

    test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var indexPost = require('../routes/index/post');
 describe('Post', function() {
     describe('create', function() {
         it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));  
+            assert.equal(-1, [1,2,3].indexOf(4));
         });
     });
 });
